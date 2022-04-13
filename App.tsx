684c89e8b14caef209b89a3362ae249237c9a1c9
Jesus <jesus.fdz93@gmail.com>
2022-04-12 22:07:48 -0500

    menu lateral basico

--- App.tsx
+++ App.tsx
@@ -2,12 +2,12 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { StackNavigator } from './src/navigator/StackNavigator';
+import { MenuLateralBasico } from './src/navigator/MenuLateralBasico';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <MenuLateralBasico />
     </NavigationContainer>
   );
 };
